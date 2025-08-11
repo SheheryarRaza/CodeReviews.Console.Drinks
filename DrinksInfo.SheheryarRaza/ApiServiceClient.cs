d7f991e3983d56671e01fd0385348295690d1438
SheheryarRaza <117226030+SheheryarRaza@users.noreply.github.com>
2025-08-11 21:30:44 +0500

    Removed Unnessecary usings

--- DrinksInfo.SheheryarRaza/ApiServiceClient.cs
+++ DrinksInfo.SheheryarRaza/ApiServiceClient.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
 using DrinksInfo.SheheryarRaza.DTOs;
 using DrinksInfo.SheheryarRaza.Models;
 using Spectre.Console;
--- DrinksInfo.SheheryarRaza/Program.cs
+++ DrinksInfo.SheheryarRaza/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DrinksInfo.SheheryarRaza;
+﻿using DrinksInfo.SheheryarRaza;
 using DrinksInfo.SheheryarRaza.Models;
 
 public class Program
